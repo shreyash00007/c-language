d20cbd0aa7ae3ce9da49f324dcf67a30973b1d17
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2020-12-10 20:34:50 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/**#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 1;
+    while( i <= 6){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
+this is for understand that how the for loop works by
+the example of while loop
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=1; i <= 5; i++){
+    printf("%d\n",i);
+}
+}
