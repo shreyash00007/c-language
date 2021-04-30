4e2243d54c12431582b91ff5d97ce9a9451c1b6f
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-04-30 13:13:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Do... While Loop*/
+/*
+int main()
+{
+    int i = 1;
+    do{
+       printf("Hello World\n");
+       i++;
+    }while(i<=3);
+    return 0;
+}
+*/
+int main()
+{
+    int i = 1;
+    do{
+       printf("%i\n",i);
+       i++;
+    }while(i<=6);
+    return 0;
+}
