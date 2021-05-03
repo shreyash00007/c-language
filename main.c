8bb8ffdf63568a4948b4a8d0aa5cf62c7ab23da2
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-03 18:45:23 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    float height, inch;
+    printf("Enter height in feet :- ");
+    scanf("%f", &height);
+    inch = height * 12;
+    printf("%f", inch);
+    return 0;
+}
+*/
+int main()
+{
+    float height, feet;
+    printf("Enter height in inch :- ");
+    scanf("%f", &height);
+    feet = height * 12;
+    printf("%f", feet);
+    return 0;
+}
