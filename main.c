309a5b219ce18f4f6b78eb72a45e2437e27e0afc
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-06-01 19:28:56 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+/**#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    sayhi("mike",19);
+    sayhi("yogesh",18);
+    sayhi("ishan",19);
+    return 0;
+}
+
+void sayhi(char name[],int age[])
+{
+    printf("hello %s and your age is %d\n",name ,age);
+}
+*/
+
+
+
+
+/**#include <stdio.h>
+
+double cube(int num);
+int main()
+{
+    printf("Answer: %f", cube(3.0));
+    return 0;
+
+}
+
+double cube(int num)
+{
+    double result = num * num * num;
+    return result;
+}
+*/
+
+
